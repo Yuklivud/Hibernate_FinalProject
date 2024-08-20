bacb575426deb3a884b8161ebbde09055c6f205b
Maksym Shkarbun <suprime28@yahoo.com>
2024-08-20 19:00:55 +0300

    Add Language.java

---
+++ src/main/java/org/hiber/country/redis/Language.java
@@ -0,0 +1,33 @@
+package org.hiber.country.redis;
+
+import java.math.BigDecimal;
+
+public class Language {
+    private String language;
+    private Boolean isOfficial;
+    private BigDecimal percentage;
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+
+    public Boolean getOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(Boolean official) {
+        isOfficial = official;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
